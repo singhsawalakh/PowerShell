363ee03e479fb45290ea875b01dea36cfaa64e5b
singhsawalakh <singh.sawalakh@gmail.com>
2023-07-23 17:52:42 +0530

    Update Keep-Active.ps1

--- Keep-Active.ps1
+++ Keep-Active.ps1
@@ -1,4 +1,26 @@
-﻿while ($true)
+﻿<#
+.SYNOPSIS
+This script will keep the computer session active by emulating NumLock key press in ever 120 seconds.
+
+.DESCRIPTION
+This script is designed to ensure that the NumLock key remains in an active state (on) consistently every 120 seconds, regardless of its previous status. 
+It does this by checking the NumLock status, sending the NumLock keypress accordingly, and then sleeping for 120 seconds before repeating the process. 
+
+.PARAMETER ParameterName
+N/A
+
+.EXAMPLE
+Example of how to use the script:
+.\Keep-Active.ps1
+
+.NOTES
+Author: Rattandeep Singh
+Date: 23 Jul 2023
+Version: V.0.1
+#>
+
+
+while ($true)
 {
     $numlockstatus = [console]::NumberLock
     if($numlockstatus -eq $false)
